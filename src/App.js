96f03d1b3e23c7b5813cb8c5bfae91d41117ec99
Tancredi M.S <tancredi620@gmail.com>
2023-03-09 16:02:55 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+	return (
+		
+		<div className="App">
+			<select>
+        <option>Cat</option>
+        <option>Fire</option>
+        <option>Futurama</option>
+        <option>Smart</option>
+        <option>There-it-is</option>
+      </select>
+   
+      
+
+
+		</div>
+	);
+}
+
+export default App;
